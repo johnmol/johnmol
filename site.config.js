d626f8506dd96446b324b6b045a7b11b36f56aaf
John M <john.withabusiness@gmail.com>
2023-08-20 16:52:53 +0200

    update config values

--- site.config.js
+++ site.config.js
@@ -3,12 +3,12 @@ const CONFIG = {
   profile: {
     name: "johnmola",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
+    role: "cybersecurity researcher",
+    bio: "CyberSecurity Researcher and Developer.",
     email: "john.withabusiness@gmail.com",
     linkedin: "johnmoaltlhegi",
     github: "johnmol",
-    instagram: "",
+    instagram: "johnmglobal",
   },
   projects: [
     {
@@ -24,7 +24,7 @@ const CONFIG = {
 
   // CONFIG configration (required)
   link: "https://johnmol.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
