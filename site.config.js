a41bbce44fd2d6a456d4f66077b304fa435970a5
John M <john.withabusiness@gmail.com>
2023-08-20 16:33:40 +0200

    change repo value

--- site.config.js
+++ site.config.js
@@ -49,7 +49,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
+      repo: "johnmol/johnmol",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
